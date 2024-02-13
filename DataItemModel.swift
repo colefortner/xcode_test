56abf482bdcb48d8b1458091b7734301a3c821a1
colefortner <colefortner@gmail.com>
2024-02-13 12:53:02 -0600

    Create DataItemModel

---
+++ DataItemModel.swift
@@ -0,0 +1,13 @@
+//
+//  DataItemModel.swift
+//  Greetings
+//
+//  Created by Cole Fortner on 2/13/24.
+//
+
+import SwiftUI
+
+struct DataItemModel {
+    let text: String
+    let color: Color
+}
