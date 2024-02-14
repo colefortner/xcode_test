3a3f6282b765029c1b732d8c22a3b28ccc114299
colefortner <colefortner@gmail.com>
2024-02-13 21:15:33 -0600

    Use ForEach to display array data using a TextView template

--- Greetings/ContentView.swift
+++ Greetings/ContentView.swift
@@ -29,12 +29,10 @@ struct ContentView: View {
 
     var body: some View {
         VStack(alignment: .leading) {
-
-            TextView(text: messages[0].text, color: messages[0].color)
-            TextView(text: messages[1].text, color: messages[1].color)
-            TextView(text: messages[2].text, color: messages[2].color)
-            TextView(text: messages[3].text, color: messages[3].color)
-            TextView(text: messages[4].text, color: messages[4].color)
+            
+            ForEach(messages) { dataItem in
+                TextView(text: dataItem.text, color: dataItem.color)
+            }
         }
         .padding()
     }
